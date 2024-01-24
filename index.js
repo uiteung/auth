e24054eecf728bb99756e7d8433beae7651168fd
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-24 15:57:41 +0700

    getCookie('login')

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.
 import {onClick,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
 
-let cookievalue= getCookie('login');
-setInner("cookiestext",cookievalue);
+
+setInner("cookiestext",getCookie('login'));
 onClick("copybutton",copyText);
 
 function copyText() {
-    navigator.clipboard.writeText(cookievalue);
+    navigator.clipboard.writeText(getCookie('login'));
 }
